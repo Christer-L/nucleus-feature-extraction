b33b8066139ee7f17ea3b5d7d6a555f68174d4f0
Christer-L <christer.lohk@gmail.com>
2022-08-26 14:25:39 +0200

    Add comments to util functions and modify feature extraction pipeline

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-from utils.utils import extract_features, prepare_data, prepare_paths
+from utils.utils import extract_features, prepare_data, prepare_paths, numpy_to_itk, get_params_from_yaml
 
 PARAMS_PATH = "/home/clohk/projects/nucleus_feature_extraction/src/params.yaml"
 DATA_DIR = "/home/clohk/data/real_and_synthetic_nuclei"
@@ -12,7 +12,13 @@ if __name__ == "__main__":
     img_paths, synthetic_img_paths, mask_paths = prepare_paths(DATA_DIR)
     filenames = [os.path.basename(path[0:-14]) for path in img_paths]
     real_images, masks = prepare_data(img_paths, mask_paths)
+
     synthetic_images, masks = prepare_data(synthetic_img_paths, mask_paths)
+    synthetic_images = numpy_to_itk(synthetic_images)
+    masks = numpy_to_itk(masks)
+    real_images, _ = prepare_data(img_paths, mask_paths)
+    real_images = numpy_to_itk(real_images)
+
 
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y_%H_%M_%S")
@@ -24,7 +30,7 @@ if __name__ == "__main__":
         TABLE_OUTPUT_DIR, "{}-{}.csv".format(dt_string, "synthetic")
     )
 
-    extract_features(PARAMS_PATH, real_images, masks, filenames, table_file_name_real)
+    extract_features(get_params_from_yaml(PARAMS_PATH), real_images, masks, filenames, table_file_name_real)
     extract_features(
-        PARAMS_PATH, synthetic_images, masks, filenames, table_file_name_synthetic
+        get_params_from_yaml(PARAMS_PATH), synthetic_images, masks, filenames, table_file_name_synthetic
     )
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,9 +8,15 @@ import SimpleITK as sitk
 import tifffile as tfile
 from radiomics import featureextractor
 from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import bios
 
 
 def prepare_paths(data_dir: str):
+    '''
+    Return paths for the sample dataset.
+    '''
     real_img_paths = glob(os.path.join(data_dir, "*_real_voxel.tif"))
     stems = [
         real_img_path[: -len("_real_voxel.tif")] for real_img_path in real_img_paths
@@ -20,7 +26,10 @@ def prepare_paths(data_dir: str):
     return real_img_paths, fake_img_paths, mask_paths
 
 
-def prepare_data(image_paths: list, mask_paths: list):
+def prepare_data_itk(image_paths: list, mask_paths: list):
+    '''
+    Return lists of images and corresponding masks in SimpleITK format.
+    '''
     images = [
         sitk.ReadImage(image_path, imageIO="TIFFImageIO") for image_path in image_paths
     ]
@@ -30,20 +39,69 @@ def prepare_data(image_paths: list, mask_paths: list):
     return images, masks
 
 
+def prepare_data(image_paths: list, mask_paths:list):
+    '''
+    Return lists of images and corresponding masks in numpy format.
+    '''
+    images = [np.array(tfile.imread(image_path)) for image_path in image_paths]
+    masks = [np.array(tfile.imread(mask_path)).astype(int) for mask_path in mask_paths]
+    return images, masks
+
+
+def numpy_to_itk(images):
+    '''
+    Convert a list of images (or masks) from numpy to SimpleITK format.
+    '''
+    return [sitk.GetImageFromArray(np.squeeze(img)) for img in images]
+
+
 def load_features(table_path) -> pd.DataFrame:
+    '''
+    Load features as pandas table from a tab-separated csv file.
+    '''
     return pd.read_csv(table_path, sep="\t")
 
 
+def get_params_from_yaml(params_path):
+    '''
+    Read feature extraction parameters from yaml file into python dict.
+    '''
+    return bios.read(params_path)
+
+
+# Feature extraction returns OrderedDict which is then converted to pd.DataFrame.
+# TODO: Check if numpy dims of the image have to be reversed as in documentation.
+# TODO: Check if features need to be returned in individual dataframes
 def extract_features(
-    params_path, images: list, masks: list, file_names: list, save_to=None
-) -> pd.DataFrame:
+        params: dict, images: list, masks: list, file_names: list, save_to=None, merge_features=True
+):
+    '''
+    Use image segmentations to extract features of the nuclei.
+        
+        Parameters:
+            params (dict): Feature extraction parameters from the parameter file 
+            (https://pyradiomics.readthedocs.io/en/latest/customization.html#parameter-file)
+           
+            images (list[ndarray]): a list of 3D images in numpy ndarray format
+           
+            masks (list[ndarray]): a list of corresponding 3D masks in numpy ndarray format.
+
+            file_names (list[str]): a list of image names associated with their features.
+
+            save_to (str): a path of a directory where the results are saved.
+
+        Returns:
+            df (DataFrame): a table of features with their corresponding images.
+    '''
     metrics = []
     labels = []
-    extractor = featureextractor.RadiomicsFeatureExtractor(params_path)
+    extractor = featureextractor.RadiomicsFeatureExtractor()
+    extractor._applyParams(paramsDict=params)
+
+    '''Return features in a single dataframe'''
     for i, mask in enumerate(masks):
         print("Image {}/{}".format(i + 1, len(masks)))
-        # Get all nuclei label values
-        mask_values = np.unique(mask)
+        mask_values = np.unique(mask) # Get all nuclei label values 
         with tqdm(total=len(mask_values) - 1, colour="green") as pbar:
             for nucleus_label in mask_values:
                 if nucleus_label == 0:
@@ -61,11 +119,8 @@ def extract_features(
                 metrics.append(values)
                 pbar.update(1)
     df = pd.DataFrame(metrics, columns=cols + ["label", "filename"])
-    if save_to is not None:
+
+    if save_to is not None and merge_features:
         df.to_csv(save_to, sep="\t", encoding="utf-8")
     return df
 
-
-def get_image_info(path):
-    img = sitk.ReadImage(path, imageIO="TIFFImageIO")
-    print("Origin: {}, Spacing: {}".format(img.GetOrigin(), img.GetSpacing()))
